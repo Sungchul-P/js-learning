c5e579434a1cbcd20741466aad081c2619673c22
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-06-20 22:32:10 +0900

    feat: CH03_05. 라우터? 화면 처리기 만들기
    라우터를 사용하기 위해, 화면을 출력하는 코드를 각각 함수로 묶어주고 리팩토링을 진행했습니다.

--- JS_TS_Essential/Hacker_News_Client/app.js
+++ JS_TS_Essential/Hacker_News_Client/app.js
@@ -5,43 +5,56 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
 function getData(url) {
-    ajax.open('GET', url, false); // 동기적으로 처리
-    ajax.send();
+  ajax.open('GET', url, false); // 동기적으로 처리
+  ajax.send();
 
-    return JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener('hashchange', function() {
-    const id = location.hash.substr(1);
+  newsList.push('<ul>');
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
-
-    title.innerHTML = newsContent.title;
-
-    content.appendChild(title);
-    console.log(newsContent);
-
-});
-
-for(let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-
-    div.innerHTML = `
-    <li>
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+      <li>
         <a href="#${newsFeed[i].id}">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
-    </li>
-    `;
+      </li>
+    `)
+  }
 
-    ul.appendChild(div.firstElementChild);
+  newsList.push('</ul>');
+
+  container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
