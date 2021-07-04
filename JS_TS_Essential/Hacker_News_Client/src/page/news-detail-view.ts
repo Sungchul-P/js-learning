6c25bb205f81be59c91c62b8256f7c8605a88df1
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-07-05 00:59:57 +0900

    refactor: CH04_09. XHR to Fetch & Promise

--- JS_TS_Essential/Hacker_News_Client/src/page/news-detail-view.ts
+++ JS_TS_Essential/Hacker_News_Client/src/page/news-detail-view.ts
@@ -43,15 +43,18 @@ export default class NewsDetailView extends View {
   render() {
     const id = location.hash.substr(7);
     const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
-    const newsDetail: NewsDetail = api.getData();
+    api.getDataWithPromise((data: NewsDetail) => {
+      const { title, content, comments} = data;
+  
+      this.store.makeRead(Number(id));
+  
+      this.setTemplateData('currentPage', this.store.currentPage.toString());
+      this.setTemplateData('title', title);
+      this.setTemplateData('content', content);
+      this.setTemplateData('comments', this.makeComment(comments));
 
-    this.store.makeRead(Number(id));
-
-    this.setTemplateData('comments', this.makeComment(newsDetail.comments));
-    this.setTemplateData('currentPage', String(this.store.currentPage));
-    this.setTemplateData('title', newsDetail.title);
-    this.setTemplateData('content', newsDetail.content);
-    this.updateView();
+      this.updateView();
+    })
   }
 
   makeComment(comments: NewsComment[]): string {
--- JS_TS_Essential/Hacker_News_Client/src/page/news-feed-view.ts
+++ JS_TS_Essential/Hacker_News_Client/src/page/news-feed-view.ts
@@ -37,14 +37,22 @@ export default class NewsFeedView extends View {
     this.store = store
     this.api = new NewsFeedApi(NEWS_URL);
 
-    if (!this.store.hasFeeds) {
-      this.store.setFeeds(this.api.getData())
-    }
   }
-
+  
   render(): void {
     this.store.currentPage = Number(location.hash.substr(7) || 1);
+    
+    if (!this.store.hasFeeds) {
+      this.api.getDataWithPromise((feeds: NewsFeed[]) => {
+        this.store.setFeeds(feeds);
+        this.renderView();
+      })
+    }
+    
+    this.renderView();
+  }
 
+  renderView = () => {
     for (let i = (this.store.currentPage - 1) * 10; i < this.store.currentPage * 10; i++) {
       const { id, title, comments_count, user, points, time_ago, read } = this.store.getFeed(i);
       this.addHtml(`
